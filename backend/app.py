3da91b41b334f086ce70ec3f109357ad97138182
Mairo022 <mairo022@outlook.com>
2024-02-06 16:55:20 +0200

    Fix caching on non 0 offset

--- backend/app.py
+++ backend/app.py
@@ -12,18 +12,19 @@ app.last_scrape_data = None
 
 @app.route("/api/jobs", methods=['GET'])
 def jobs_data():
-    if time.time() - app.last_scrape_time < app.cache_time:
-        return jsonify(app.last_scrape_data)
-
     start = request.args.get("start", 0)
+
+    if start == "0" and time.time() - app.last_scrape_time < app.cache_time:
+        return jsonify(app.last_scrape_data)
 
     cv_keskus = scraper.get_jobs_cv_keskus(start)
     cv = scraper.get_jobs_cv(start)
 
     combined = {"cv_keskus": cv_keskus, "cv": cv}
 
-    app.last_scrape_time = time.time()
-    app.last_scrape_data = combined
+    if start == "0":
+        app.last_scrape_time = time.time()
+        app.last_scrape_data = combined
 
     return jsonify(combined)
 
