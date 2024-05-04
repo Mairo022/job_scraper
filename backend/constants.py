dae280cf9e22be499d3937779047f56a818ee338
Mairo022 <mairo022@outlook.com>
2024-05-04 11:32:51 +0300

    Handle 'days left' case

--- backend/constants.py
+++ backend/constants.py
@@ -11,6 +11,8 @@
 # 9 - Viljandi
 # 10 - Võru
 
+ADS_LIMIT = 30
+
 LOCATIONS_AVAILABLE = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
 LOCATIONS_CV = {
--- backend/scraper.py
+++ backend/scraper.py
@@ -3,7 +3,7 @@ import requests
 import timeConverter
 import traceback
 from bs4 import BeautifulSoup
-from constants import LOCATIONS_CVK, LOCATIONS_CV
+from constants import LOCATIONS_CVK, LOCATIONS_CV, ADS_LIMIT
 
 
 def get_jobs_cv_keskus(start, location):
@@ -18,7 +18,7 @@ def get_jobs_cv_keskus(start, location):
 
         jobs_list = []
 
-        for job in jobs:
+        for i, job in enumerate(jobs):
             main_info = job.find("div", class_="main-info")
 
             time = main_info.find("div").find_all("span")[-1]
@@ -27,13 +27,14 @@ def get_jobs_cv_keskus(start, location):
             salary = main_info.find(class_="salary-block")
             link = job.get("href")
 
+            time_text = getTimeText(time.text, jobs, i)
             salary_text = salary.text.rsplit("\xa0", 1)[0] if salary is not None else None
             link_text = "https://www.cvkeskus.ee" + link
 
             job_dict = {
                 "position": position.text,
                 "company": company.text,
-                "time": timeConverter.convertCVKeskusToCVTimeFormat(time.text),
+                "time": timeConverter.convertCVKeskusToCVTimeFormat(time_text),
                 "salary": salary_text,
                 "link": link_text
             }
@@ -52,7 +53,7 @@ def get_jobs_cv_keskus(start, location):
 def get_jobs_cv(start, location):
     try:
         location = LOCATIONS_CV.get(location)
-        url = f"https://cv.ee/api/v1/vacancy-search-service/search?limit=30&offset={start}&towns[]={location}&fuzzy=true&suitableForRefugees=false&isHourlySalary=false&isRemoteWork=false&isQuickApply=false&sorting=LATEST&showHidden=true"
+        url = f"https://cv.ee/api/v1/vacancy-search-service/search?limit={ADS_LIMIT}&offset={start}&towns[]={location}&fuzzy=true&suitableForRefugees=false&isHourlySalary=false&isRemoteWork=false&isQuickApply=false&sorting=LATEST&showHidden=true"
         response = requests.get(url)
 
         if response.status_code == 200:
@@ -72,3 +73,21 @@ def get_jobs_cv(start, location):
         logging.error(f"CV:\n{traceback_str}")
 
         return []
+
+
+def getTimeText(time, jobs, i):
+    if time.split(" ")[-1] == "jäänud":
+        if i == 0:
+            job = jobs[i+1]
+            main_info = job.find("div", class_="main-info")
+            time = main_info.find("div").find_all("span")[-1]
+
+            return time.text
+        if i <= ADS_LIMIT:
+            job = jobs[i-1]
+            main_info = job.find("div", class_="main-info")
+            time = main_info.find("div").find_all("span")[-1]
+
+            return time.text
+
+    return time
