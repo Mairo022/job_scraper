4427659dd2366719020ae0026385a759f1d49bfe
Mairo022 <mairo022@outlook.com>
2024-05-09 19:59:27 +0300

    Extract job data handling to a function

--- backend/app.py
+++ backend/app.py
@@ -3,8 +3,6 @@ from flask import Flask, jsonify, request, abort
 from appUtils import *
 from constants import LOCATIONS_AVAILABLE, ADS_LIMIT
 
-import scraper
-import time
 
 app = Flask(__name__)
 
@@ -23,33 +21,9 @@ def jobs_data():
     if location not in LOCATIONS_AVAILABLE:
         abort(400, "Invalid location id")
 
-    if start == 0:
-        cached_data_item = app.cached_data.get(str(location))
+    job_ads = get_jobs(start, location, app.cached_data, app.real_pages[location])
 
-        if cached_data_item and is_cache_fresh(cached_data_item.get("time")):
-            return jsonify(cached_data_item.get("data"))
-
-    real_pages = app.real_pages[location]
-    start_cv, start_cvk = real_pages.get_pages(start)
-
-    cv_keskus = scraper.get_jobs_cv_keskus(start_cvk, location)
-    cv = scraper.get_jobs_cv(start_cv, location)
-
-    cv_ads_older, cvk_ads_older = evaluate_ads_age(cv, cv_keskus)
-
-    combined = {"cv_keskus": cv_keskus, "cv": cv}
-
-    real_pages.set_pages(start, start_cv, start_cvk, cv_ads_older, cvk_ads_older)
-
-    if start == 0 and cv and cv_keskus:
-        app.cached_data[str(location)] = {
-            "data": combined,
-            "time": time.time()
-        }
-
-    return_data = get_return_data(cv_ads_older, cvk_ads_older, cv, cv_keskus)
-
-    return jsonify(return_data)
+    return jsonify(job_ads)
 
 
 with app.app_context():
--- backend/appUtils.py
+++ backend/appUtils.py
@@ -2,12 +2,9 @@ import os
 import logging
 import time
 
+import scraper
 from constants import LOCATIONS_AVAILABLE, CACHE_LIFESPAN
 from pages import Pages
-
-
-def is_cache_fresh(cache_time):
-    return time.time() - cache_time < CACHE_LIFESPAN
 
 
 def initialise_pages():
@@ -17,6 +14,51 @@ def initialise_pages():
         pages[location] = Pages(location)
 
     return pages
+
+
+def get_jobs(offset, location, cache, real_pages):
+    if offset == 0:
+        cached_data_item = cache.get(str(location))
+
+        if cached_data_item and is_cache_fresh(cached_data_item.get("time")):
+            return cached_data_item.get("data")
+
+    start_cv, start_cvk = real_pages.get_pages(offset)
+    cvk = scraper.get_jobs_cv_keskus(start_cvk, location)
+    cv = scraper.get_jobs_cv(start_cv, location)
+    cv_ads_older, cvk_ads_older = evaluate_ads_age(cv, cvk)
+
+    real_pages.set_pages(offset, start_cv, start_cvk, cv_ads_older, cvk_ads_older)
+
+    if offset == 0 and cv and cvk:
+        cache[str(location)] = {
+            "data": {"cv_keskus": cvk, "cv": cv},
+            "time": time.time()
+        }
+
+    job_ads = assign_jobs(cv_ads_older, cvk_ads_older, cv, cvk)
+
+    return job_ads
+
+
+def initialise_logging():
+    log_folder = os.path.join(os.getcwd(), 'logs')
+
+    if not os.path.exists(log_folder):
+        os.makedirs(log_folder)
+
+    log_file = os.path.join(log_folder, "app.log")
+    logging.basicConfig(filename=log_file, level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+def assign_jobs(cv_ads_older, cvk_ads_older, cv, cv_keskus):
+    if cv_ads_older:
+        return {"cv_keskus": cv_keskus, "cv": []}
+
+    if cvk_ads_older:
+        return {"cv_keskus": [], "cv": cv}
+
+    return {"cv_keskus": cv_keskus, "cv": cv}
 
 
 def evaluate_ads_age(cv, cvk):
@@ -31,21 +73,5 @@ def evaluate_ads_age(cv, cvk):
     return [cv_ads_older, cvk_ads_older]
 
 
-def get_return_data(cv_ads_older, cvk_ads_older, cv, cv_keskus):
-    if cv_ads_older:
-        return {"cv_keskus": cv_keskus, "cv": []}
-
-    if cvk_ads_older:
-        return {"cv_keskus": [], "cv": cv}
-
-    return {"cv_keskus": cv_keskus, "cv": cv}
-
-
-def initialise_logging():
-    log_folder = os.path.join(os.getcwd(), 'logs')
-
-    if not os.path.exists(log_folder):
-        os.makedirs(log_folder)
-
-    log_file = os.path.join(log_folder, "app.log")
-    logging.basicConfig(filename=log_file, level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
+def is_cache_fresh(cache_time):
+    return time.time() - cache_time < CACHE_LIFESPAN
