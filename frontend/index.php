da70e4d78e345240f1bdd7f072783ae2d320caac
Mairo022 <mairo022@outlook.com>
2024-09-25 17:47:52 +0300

    Style adjustments

--- frontend/index.php
+++ frontend/index.php
@@ -21,7 +21,7 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./style.css?v102">
     <link rel="icon" href="./favicon.ico" type="image/x-icon">
     <script defer src="script.js"></script>
     <title>Jobs</title>
