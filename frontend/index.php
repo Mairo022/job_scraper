68d23cd989ec6d5509f72393c5fcc09e4a2fa612
Mairo022 <mairo022@outlook.com>
2024-10-08 10:05:34 +0300

    Add custom scrollbars

--- frontend/index.php
+++ frontend/index.php
@@ -30,7 +30,7 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style.css?v102">
+    <link rel="stylesheet" href="./style.css?v103">
     <link rel="icon" href="./favicon.ico" type="image/x-icon">
     <script defer src="script.js"></script>
     <title>Jobs</title>
