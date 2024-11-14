a159e5b5990df8536e1a72f07f4010af368760be
Mairo022 <mairo022@outlook.com>
2024-11-14 11:21:59 +0200

    Switch to class for real pages

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,14 @@
 from flask import Flask, jsonify, request, abort
 
 from RateLimiter import RateLimiter
+from RealPages import RealPages
 from appUtils import *
 from constants import LOCATIONS_AVAILABLE, CATEGORIES_AVAILABLE, ADS_LIMIT, LOCALHOST_IP
 
 app = Flask(__name__)
 
 app.cached_data = dict()
-app.real_pages = initialise_pages()
+app.real_pages = RealPages().pages
 app.rate_limiter = RateLimiter()
 
 
