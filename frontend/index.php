13a8b858549a5e58d71e488d683e83791643835e
Mairo022 <mairo022@outlook.com>
2025-06-10 09:01:26 +0300

    Upgrade design

--- frontend/index.php
+++ frontend/index.php
@@ -24,9 +24,9 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style.css?v107">
+    <link rel="stylesheet" href="./style.css?v108">
     <link rel="icon" href="./favicon.ico" type="image/x-icon">
-    <script defer src="script.js?v106"></script>
+    <script defer src="script.js?v107"></script>
     <title>Jobs</title>
   </head>
   <body>
@@ -36,19 +36,11 @@
         </header>
         <section class="jobs">
             <div class="filters">
-                <a class="it_filter" href='<?= $categoryID == 0 ? "?location=0&category=1" : "?location={$locationID}&category=0 " ?>'>
-                    <div class="it_filter__status"></div>
-                    <span class="it_filter__text">IT töö</span>
-                </a>
-                <button class="saved_filter">
-                    <div class="saved_filter__status"></div>
-                    <span class="saved_filter__text">Salvestatud</span>
-                </button>
-                <div class="location-menu">
+                <div class="location_filter">
                     <div class="location__select">
                         <div class="location__select-active"><?= LOCATION_DEFAULT ?></div>
                         <div class="location__select-button">
-                            <svg fill="#444" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 330 330" xml:space="preserve"><path id="XMLID_222_" d="M250.606,154.389l-150-149.996c-5.857-5.858-15.355-5.858-21.213,0.001 c-5.857,5.858-5.857,15.355,0.001,21.213l139.393,139.39L79.393,304.394c-5.857,5.858-5.857,15.355,0.001,21.213 C82.322,328.536,86.161,330,90,330s7.678-1.464,10.607-4.394l149.999-150.004c2.814-2.813,4.394-6.628,4.394-10.606 C255,161.018,253.42,157.202,250.606,154.389z"></path></svg>                    
+                            <svg fill="#444" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 330 330" xml:space="preserve"><path id="XMLID_222_" d="M250.606,154.389l-150-149.996c-5.857-5.858-15.355-5.858-21.213,0.001 c-5.857,5.858-5.857,15.355,0.001,21.213l139.393,139.39L79.393,304.394c-5.857,5.858-5.857,15.355,0.001,21.213 C82.322,328.536,86.161,330,90,330s7.678-1.464,10.607-4.394l149.999-150.004c2.814-2.813,4.394-6.628,4.394-10.606 C255,161.018,253.42,157.202,250.606,154.389z"></path></svg>
                         </div>
                     </div>
                     <ul class="location__options">
@@ -60,7 +52,15 @@
                             </li>
                         <?php endforeach; ?>
                     </ul>
-                </div>                
+                </div>
+                <a class="filter" href='<?= $categoryID == 0 ? "?location=0&category=1" : "?location={$locationID}&category=0 " ?>'>
+                    <div class="filter__status" id="it_filter_status"></div>
+                    <span class="filter__text">IT Töö</span>
+                </a>
+                <button class="filter" id="saved_view_btn">
+                    <span class="filter__status" id="saved_view_status"></span>
+                    <span class="filter__text filter__text">Salvestatud</span>
+                </button>
             </div>
             <ul>
             <?php if ($responseCode !== 200): ?>
@@ -81,7 +81,7 @@
                                 <span class="job__row__detail"><?= format_time($job->publishDate) ?></span>
                             </div>
                         </div>
-                        <div class="job__save">Salvesta</div>
+                        <button type="button" class="job__save" aria-label="Salvesta"><svg fill="#000000" height="34px" width="34px" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve"><g><g><g><path d="M426.027,209.813c-1.493-4.267-5.547-7.147-10.027-7.147H311.36l-40.213-95.787c-2.24-5.44-8.533-8-13.973-5.653 c-2.56,1.067-4.587,3.093-5.653,5.653l-40.213,95.787h-104.64c-5.867,0-10.667,4.8-10.667,10.667c0,3.2,1.493,6.187,3.947,8.213 L184,289.92l-35.84,112.427c-1.813,5.653,1.28,11.627,6.933,13.44c3.307,1.067,6.827,0.427,9.6-1.6l96.64-71.787l96.747,72.533 c4.693,3.52,11.413,2.56,14.933-2.133c2.027-2.773,2.667-6.293,1.6-9.6l-35.947-113.28l84.053-68.267 C426.24,218.773,427.52,213.973,426.027,209.813z M319.573,277.867c-3.413,2.773-4.8,7.36-3.413,11.52l28.053,88.533 l-76.48-57.28c-3.84-2.88-8.96-2.88-12.8,0l-76.587,56.747L206.4,289.6c1.387-4.16,0-8.747-3.413-11.52L136.64,224h81.707 c4.267,0,8.213-2.56,9.813-6.507l33.173-78.827l33.067,78.827c1.707,3.947,5.547,6.507,9.813,7.573h81.707L319.573,277.867z"/><path d="M256,0C114.88,0,0,114.88,0,256s114.88,256,256,256s256-114.88,256-256S397.12,0,256,0z M256,490.667 c-129.387,0-234.667-105.28-234.667-234.667S126.613,21.333,256,21.333S490.667,126.613,490.667,256S385.387,490.667,256,490.667 z"/></g></g></g></svg></button>
                     </li>
                 <?php endif; ?>
                 <?php if (isset($job->position)): ?>
@@ -96,7 +96,7 @@
                                 <span class="job__row__detail"><?= format_time($job->time) ?></span>
                             </div>
                         </div>
-                        <div class="job__save">Salvesta</div>
+                        <button type="button" class="job__save" aria-label="Salvesta"><svg fill="#000000" height="34px" width="34px" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve"><g><g><g><path d="M426.027,209.813c-1.493-4.267-5.547-7.147-10.027-7.147H311.36l-40.213-95.787c-2.24-5.44-8.533-8-13.973-5.653 c-2.56,1.067-4.587,3.093-5.653,5.653l-40.213,95.787h-104.64c-5.867,0-10.667,4.8-10.667,10.667c0,3.2,1.493,6.187,3.947,8.213 L184,289.92l-35.84,112.427c-1.813,5.653,1.28,11.627,6.933,13.44c3.307,1.067,6.827,0.427,9.6-1.6l96.64-71.787l96.747,72.533 c4.693,3.52,11.413,2.56,14.933-2.133c2.027-2.773,2.667-6.293,1.6-9.6l-35.947-113.28l84.053-68.267 C426.24,218.773,427.52,213.973,426.027,209.813z M319.573,277.867c-3.413,2.773-4.8,7.36-3.413,11.52l28.053,88.533 l-76.48-57.28c-3.84-2.88-8.96-2.88-12.8,0l-76.587,56.747L206.4,289.6c1.387-4.16,0-8.747-3.413-11.52L136.64,224h81.707 c4.267,0,8.213-2.56,9.813-6.507l33.173-78.827l33.067,78.827c1.707,3.947,5.547,6.507,9.813,7.573h81.707L319.573,277.867z"/><path d="M256,0C114.88,0,0,114.88,0,256s114.88,256,256,256s256-114.88,256-256S397.12,0,256,0z M256,490.667 c-129.387,0-234.667-105.28-234.667-234.667S126.613,21.333,256,21.333S490.667,126.613,490.667,256S385.387,490.667,256,490.667 z"/></g></g></g></svg></button>
                     </li>
                 <?php endif; ?>
             <?php endforeach; ?>
@@ -107,7 +107,7 @@
                     class="jobs__paging__page<?= $offset == 0 ? ' disabled' : '' ?>" 
                     href='<?= "?location={$locationID}&start={$offsetPrevious}&category={$categoryID}" ?>'
                     >
-                    <svg class="jobs__paging__page__svg" strokeWidth={0.8} stroke="currentColor" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" xmlSpace="preserve">
+                    <svg class="jobs__paging__page__svg" stroke="currentColor" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path d="M17.2 23.7 5.4 12 17.2.3l1.3 1.4L8.4 12l10.1 10.3z" />
                     </svg>
                 </a>
@@ -115,13 +115,15 @@
                     class="jobs__paging__page" 
                     href='<?= "?location={$locationID}&start={$offsetNext}&category={$categoryID}" ?>'
                     >
-                    <svg class="jobs__paging__page__svg"strokeWidth={0.8} stroke="currentColor" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" xmlSpace="preserve">
+                    <svg class="jobs__paging__page__svg" stroke="currentColor" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path d="M6.8 0.3 18.6 12 6.8 23.7 5.5 22.3 15.6 12 5.5 1.7z" />
                     </svg>
                 </a>
                 </div>
             <?php endif; ?>
         </section>
+        <div class="glow_bg"></div>
+        <div class="stars_bg"></div>
     </main>
   </body>
 </html>
--- frontend/script.js
+++ frontend/script.js
@@ -3,9 +3,9 @@ const locationSelectText = document.querySelector(".location__select-active")
 const locationSelectButton = document.querySelector(".location__select-button")
 const locationOptions = document.querySelector(".location__options")
 
-const itFilterStatus = document.querySelector(".it_filter__status")
+const itFilterStatus = document.querySelector("#it_filter_status")
 
-const savedButton = document.querySelector(".saved_filter") 
+const savedButton = document.querySelector("#saved_view_btn")
 let isSavedView = false
 
 const urlParams = new URLSearchParams(window.location.search)
@@ -92,7 +92,6 @@ function handleSaveButtonClick(e) {
 
     if (doUnsave) {
         e.target.classList.remove("saved")
-        e.target.innerText = "Salvesta"
         
         const savedJobsCleaned = savedJobs.filter(job => job?.url != url)
         localStorage.setItem("saved", JSON.stringify(savedJobsCleaned))
@@ -108,7 +107,6 @@ function handleSaveButtonClick(e) {
     }
 
     e.target.classList.add("saved")
-    e.target.innerText = "Salvestatud"
 
     savedJobs.push({title, company, url, added: new Date()})
     localStorage.setItem("saved", JSON.stringify(savedJobs))
@@ -126,7 +124,6 @@ function assignUnsavedJobStatus() {
 
             if (jobSaveEl.classList.contains("saved")) {
                 jobSaveEl.classList.remove("saved")
-                jobSaveEl.innerText = "Salvesta"
             }
         }
 
@@ -140,7 +137,6 @@ function assignUnsavedJobStatus() {
 
         if (!isSaved && jobSaveEl.classList.contains("saved")) {
             jobSaveEl.classList.remove("saved")
-            jobSaveEl.innerText = "Salvesta"
         }
     }   
 }
@@ -160,7 +156,6 @@ function assignSavedJobStatus() {
             const jobSaveEl = job.querySelector(".job__save")
 
             jobSaveEl.classList.add("saved")
-            jobSaveEl.innerText = "Salvestatud"
 
             assigned++
         }
@@ -175,7 +170,7 @@ function handleSavedViewButtonClick() {
 
     isSavedView = !isSavedView
 
-    const savedButtonStatus = savedButton.querySelector(".saved_filter__status")
+    const savedButtonStatus = savedButton.querySelector("#saved_view_status")
     
     if (isSavedView) savedButtonStatus.classList.add("active")
     else savedButtonStatus.classList.remove("active")
@@ -206,7 +201,7 @@ function createSavedView() {
                         <span class="job__row__detail">Salvestatud ${formatTime(job.added)}</span>
                     </div>
                 </div>
-                <div class="job__save saved">Eemalda</div>
+                <div class="job__save saved delete"><svg viewBox="0 0 24 24" height="34px" width="34px" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M20.5001 6H3.5" stroke="#000000" stroke-width="1.5" stroke-linecap="round"></path> <path d="M18.8332 8.5L18.3732 15.3991C18.1962 18.054 18.1077 19.3815 17.2427 20.1907C16.3777 21 15.0473 21 12.3865 21H11.6132C8.95235 21 7.62195 21 6.75694 20.1907C5.89194 19.3815 5.80344 18.054 5.62644 15.3991L5.1665 8.5" stroke="#000000" stroke-width="1.5" stroke-linecap="round"></path> <path d="M6.5 6C6.55588 6 6.58382 6 6.60915 5.99936C7.43259 5.97849 8.15902 5.45491 8.43922 4.68032C8.44784 4.65649 8.45667 4.62999 8.47434 4.57697L8.57143 4.28571C8.65431 4.03708 8.69575 3.91276 8.75071 3.8072C8.97001 3.38607 9.37574 3.09364 9.84461 3.01877C9.96213 3 10.0932 3 10.3553 3H13.6447C13.9068 3 14.0379 3 14.1554 3.01877C14.6243 3.09364 15.03 3.38607 15.2493 3.8072C15.3043 3.91276 15.3457 4.03708 15.4286 4.28571L15.5257 4.57697C15.5433 4.62992 15.5522 4.65651 15.5608 4.68032C15.841 5.45491 16.5674 5.97849 17.3909 5.99936C17.4162 6 17.4441 6 17.5 6" stroke="#000000" stroke-width="1.5"></path> </g></svg></div>
             </li>
         `)
     })
